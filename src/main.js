e3be5595be4a3b291d49262d842c09cda351cb0f
Guru <guru.sg@hotmail.com>
2020-06-26 20:55:59 +0930

    changes pending
    Still yet to fix the age issue

--- src/main.js
+++
@@ -1,6 +0,0 @@
-function calculate_age(new Date(1996, 1, 1)) { 
-    var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms); 
-  
-    age = Math.abs(age_dt.getUTCFullYear() - 1970);
-}
